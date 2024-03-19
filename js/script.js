fed44b4e39b7c241b63406f13b0a5461a00991d8
Jason Marshall <jasonmarshall1994@gmail.com>
2024-03-19 18:10:41 -0400

    Added displayPopularShows function

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+// Display 20 most popular movies
+
 async function displayPopularMovies() {
   const { results } = await fetchAPIData('movie/popular');
+
   results.forEach((movie) => {
     const div = document.createElement('div');
     div.classList.add('card');
@@ -35,6 +38,42 @@ async function displayPopularMovies() {
   });
 }
 
+// Display 20 most popular TV shows
+
+async function displayPopularShows() {
+  const { results } = await fetchAPIData('tv/popular');
+
+  results.forEach((show) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="tv-details.html?id=${show.id}">
+            ${
+              show.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+                : `<img
+            src="../images/no-image.jpg"
+            class="card-img-top"
+            alt="${show.name}"
+          />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Air Date: ${show.first_air_date}</small>
+            </p>
+          </div>
+      `;
+
+    document.querySelector('#popular-shows').appendChild(div);
+  });
+}
+
 // Fetch data from TMDB API
 
 async function fetchAPIData(endpoint) {
@@ -54,9 +93,13 @@ async function fetchAPIData(endpoint) {
   return data;
 }
 
+// Show spinner
+
 function showSpinner() {
   document.querySelector('.spinner').classList.add('show');
 }
+
+// Hide spinner
 
 function hideSpinner() {
   document.querySelector('.spinner').classList.remove('show');
@@ -78,10 +121,9 @@ function init() {
     case '/':
     case '/index.html':
       displayPopularMovies();
-      console.log('Home');
       break;
     case '/shows.html':
-      console.log('Shows');
+      displayPopularShows();
       break;
     case '/movie-details.html':
       console.log('Movie Details');
